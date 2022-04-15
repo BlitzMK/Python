24464a1d763a9fd2bcabe07f08a8b984c0927785
Samuel Chandler <sec180005@utdallas.edu>
2022-04-15 00:03:07 -0500

    update

--- Excercises/HackerRank/NestedLists/NestedLists.py
+++ Excercises/HackerRank/NestedLists/NestedLists.py
@@ -1,4 +1,22 @@
+from sys import last_traceback
+
+
 if __name__ == '__main__':
+
+    Last = [[101,float(101)]]
+    second = []
+    
+    Students = []
+    currentStudent = 0
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        currentStudent = [name,score]
+        Students.append(currentStudent)
+        if currentStudent[1] < Last[0][1]:
+            second = Last
+            Last = currentStudent
+        if currentStudent[1] == Last[0][1]:
+            Last.append[currentStudent]
+        
+        print(second)
